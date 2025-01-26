f0fc1564ffa8bd60f9003b69884ec2253de434d3
ABBEY AINA <olamilekansunday445@gmail.com>
2025-01-26 17:04:20 +0000

    third commit

---
+++ alx-movie-app/components/commons/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading: React.FC = () => {
+    return (
+        <div>
+            {/* Loading Indicator */}
+        </div>
+    );
+};
+
+export default Loading;
---
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout: React.FC = () => {
+    return (
+        <div>
+            {/* Layout Content */}
+        </div>
+    );
+};
+
+export default Layout;
